b2ebd3011ef851a354587145c9b872fec5a06d58
Juan José Rubio <admin@tallernt.es>
2022-04-24 15:38:12 +0200

    Startup.sh fixes

--- startup.sh
+++ startup.sh
@@ -7,9 +7,6 @@ sudo apt install htop tmux neovim fzf silversearcher-ag git build-essential flat
 echo "Instalando omf..."
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-fish -c "omf install bobthefish"
-fish -c "omf install bass"
-
 echo "Instalando vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -20,8 +17,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 echo "Copiando dotfiles..."
 git clone https://github.com/nt-jjrubio/dotfiles.git /tmp/dotfiles/
 cp /tmp/dotfiles/.tmux.conf ~
-cp -R /tmp/dotfiles/nvim ~/.config/
-cp -R /tmp/dotfiles/fish/functions/* ~/.config/fish/functions/
+cp -R /tmp/dotfiles/.config/nvim ~/.config/
+cp -R /tmp/dotfiles/.config/fish/functions/* ~/.config/fish/functions/
 echo "load_nvm > /dev/stderr" >> ~/.config/fish/config.fish
 
 echo "Instalando nvim plugins"
@@ -29,5 +26,7 @@ nvim -c "PlugInstall" -c "qa!"
 
 echo "Cambiando shell por fish"
 chsh -s /usr/bin/fish
+
+fish -c "omf install bobthefish bass"
 
 echo -e "\e[33mInstalación terminada\e[0m"
